4a4424537e6a2cd5ae2c9a73886bf3e228fb14f8
Alfred Ssekagiri <alfred@Alfreds-MacBook-Pro.local>
2023-03-07 00:41:19 +0300

    configure custom domain

--- hivseqdb/settings.py
+++ hivseqdb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1!2rz_@lrni)v54b0xg5n1dft(6+gzutyi(+##=egg6)aq2qg0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'hivseqdb.org']
 
 # Application definition
 
